0016571f02266464b54e429aff31af79bb9cbf55
INOUE-Syun <xinoue.syun@contract.isid.co.jp>
2019-06-27 21:28:54 +0900

    hogeee

--- test_awesome.py
+++ test_awesome.py
@@ -1,20 +1,20 @@
 import awesome
 
 
-def test_smile():
-    assert awesome.smile() == ":)"
+# def test_smile():
+#     assert awesome.smile() == ":)"
 
 
 def test_frown():
     assert awesome.frown() == ":("
 
 
-def test_poker_face():
-    assert awesome.poker_face() == ":|"
+# def test_poker_face():
+#     assert awesome.poker_face() == ":|"
 
 
-def test_japanese_emoji_smile():
-    assert awesome.japanese_emoji_smile() == "^_^"
+# def test_japanese_emoji_smile():
+#     assert awesome.japanese_emoji_smile() == "^_^"
 
 
 def test_japanese_emoji_sleep():
