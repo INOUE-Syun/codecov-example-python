7585e46920f0b7f23e325625add28cebb5d55e0e
INOUE-Syun <xinoue.syun@contract.isid.co.jp>
2019-06-27 18:15:58 +0900

    add tests

--- test_awesome.py
+++ test_awesome.py
@@ -7,3 +7,15 @@ def test_smile():
 
 def test_frown():
     assert awesome.frown() == ":("
+
+
+def test_poker_face():
+    assert awesome.poker_face() == ":|"
+
+
+def test_smile():
+    assert awesome.japanese_emoji_smile() == "^_^"
+
+
+def test_sleep():
+    assert awesome.japanese_emoji_sleep() == "-_- zzz"
