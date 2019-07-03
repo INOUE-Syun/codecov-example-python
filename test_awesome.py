7db846ffc89a2753ff6f35743c1f2a1a0abf4899
INOUE-Syun <xinoue.syun@contract.isid.co.jp>
2019-07-03 15:50:21 +0900

    eee

--- test_awesome.py
+++ test_awesome.py
@@ -2,36 +2,36 @@ import awesome
 from awesome import hoge, fuga
 
 
-# def test_smile():
-#     assert awesome.smile() == ":)"
+def test_smile():
+    assert awesome.smile() == ":)"
 
 
-# def test_frown():
-#     assert awesome.frown() == ":("
+def test_frown():
+    assert awesome.frown() == ":("
 
 
-# def test_poker_face():
-#     assert awesome.poker_face() == ":|"
+def test_poker_face():
+    assert awesome.poker_face() == ":|"
 
 
-# def test_japanese_emoji_smile():
-#     assert awesome.japanese_emoji_smile() == "^_^"
+def test_japanese_emoji_smile():
+    assert awesome.japanese_emoji_smile() == "^_^"
 
 
-# def test_japanese_emoji_sleep():
-#     assert awesome.japanese_emoji_sleep() == "-_- zzz"
+def test_japanese_emoji_sleep():
+    assert awesome.japanese_emoji_sleep() == "-_- zzz"
 
 
-# def test_hoge():
-#     assert hoge.hoge() == "hoge"
+def test_hoge():
+    assert hoge.hoge() == "hoge"
 
 
-# def test_fuga():
-#     assert hoge.fuga() == "fuga"
+def test_fuga():
+    assert hoge.fuga() == "fuga"
 
 
-# def test_piyo():
-#     assert hoge.piyo() == "piyo"
+def test_piyo():
+    assert hoge.piyo() == "piyo"
 
 
 def test_foo():
